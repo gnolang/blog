9b6f1716af1100cf0656a23cb4373da5f23c7d77
Manfred Touron <94029+moul@users.noreply.github.com>
2023-02-04 00:44:23 +0100

    chore: call gnokey
    Signed-off-by: Manfred Touron <94029+moul@users.noreply.github.com>

--- gnoblog-cli/client.go
+++ gnoblog-cli/client.go
@@ -5,10 +5,15 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
-	"time"
 
-	"github.com/adrg/frontmatter"
+	"github.com/gnolang/gno/pkgs/amino"
+	"github.com/gnolang/gno/pkgs/crypto/keys"
+	"github.com/gnolang/gno/pkgs/crypto/keys/client"
+	"github.com/gnolang/gno/pkgs/sdk/vm"
+	"github.com/gnolang/gno/pkgs/std"
 	"github.com/peterbourgon/ff/v3/ffcli"
 )
 
@@ -21,58 +26,85 @@ func main() {
 	}
 }
 
-type pushOpts struct {
+type publishOpts struct {
 	Debug   bool
 	Publish bool
-	// Remote  string
-	// GnokeyOpts
+	PkgPath string
+
+	// gnokey
+	client.BaseOptions
+	GasWanted       int64
+	GasFee          string
+	ChainID         string
+	KeyNameOrBech32 string
 }
 
-func (opts *pushOpts) applyDefaults() {
-	opts.Debug = false
-	opts.Publish = false
-	// opts.Remote = "test3.gno.land:36657"
-}
+func (opts *publishOpts) flagSet() *flag.FlagSet {
+	fs := flag.NewFlagSet("blog publish", flag.ExitOnError)
+	fs.BoolVar(&opts.Debug, "debug", false, "verbose output")
+	fs.BoolVar(&opts.Publish, "publish", false, "publish blogpost")
+	fs.Int64Var(&opts.GasWanted, "gas-wanted", 1000, "gas requested for tx")
+	fs.StringVar(&opts.GasFee, "gas-fee", "1ugnot", "gas payment fee")
+	fs.StringVar(&opts.ChainID, "chainid", "test3", "")
+	fs.StringVar(&opts.PkgPath, "pkgpath", "r/gnoland/blog", "blog realm path")
 
-func (opts *pushOpts) flagSet() *flag.FlagSet {
-	fs := flag.NewFlagSet("blog push", flag.ExitOnError)
-	fs.BoolVar(&opts.Debug, "d", false, "verbose output")
-	fs.BoolVar(&opts.Publish, "p", false, "publish blogpost")
+	// client.BaseOptions
+	fs.StringVar(&opts.Home, "home", client.DefaultBaseOptions.Home, "home directory")
+	fs.StringVar(&opts.Remote, "remote", "test3.gno.land:36657", "remote node URL")
+	fs.BoolVar(&opts.Quiet, "quiet", false, "for parsing output")
+	fs.BoolVar(&opts.InsecurePasswordStdin, "insecure-password-stdin", false, "WARNING! take password from stdin")
 	return fs
 }
 
 func run(args []string) error {
-	var opts pushOpts
+	var opts publishOpts
 
 	root := &ffcli.Command{
-		ShortUsage: "blog <POST...>",
+		ShortUsage: "gnoblog-cli KEY <POST...>",
+		ShortHelp:  "(re)publish a blogpost",
 		FlagSet:    opts.flagSet(),
 		Exec: func(ctx context.Context, args []string) error {
-			posts := args
-			return doPush(ctx, posts, opts)
+			if len(args) < 2 {
+				return flag.ErrHelp
+			}
+			opts.KeyNameOrBech32 = args[0]
+			posts := args[1:]
+			return doPublish(ctx, posts, opts)
 		},
 	}
 
 	if err := root.Parse(args); err != nil {
 		return fmt.Errorf("parse error: %w", err)
 	}
+
+	if !opts.Debug {
+		log.SetOutput(ioutil.Discard)
+	}
+
 	if err := root.Run(context.Background()); err != nil {
 		return fmt.Errorf("runtime error: %w", err)
 	}
+
 	return nil
 }
 
-type postMetadata struct {
-	Title           string    `yaml:"title"`
-	Tags            []string  `yaml:"tags"`
-	Authors         []string  `yaml:"authors"`
-	PublicationDate time.Time `yaml:"publication_date"`
-	Slug            string    `yaml:"slug"`
-}
+func doPublish(ctx context.Context, posts []string, opts publishOpts) error {
+	log.Println("opts:", string(amino.MustMarshalJSON(opts)))
 
-func doPush(ctx context.Context, posts []string, opts pushOpts) error {
-	if len(posts) < 1 {
-		return flag.ErrHelp
+	kb, err := keys.NewKeyBaseFromDir(opts.Home)
+	if err != nil {
+		return err
+	}
+	info, err := kb.GetByNameOrAddress(opts.KeyNameOrBech32)
+	if err != nil {
+		return err
+	}
+	caller := info.GetAddress()
+	log.Println("key:", caller)
+
+	gasFee, err := std.ParseCoin(opts.GasFee)
+	if err != nil {
+		return fmt.Errorf("parse gas fee: %w", err)
 	}
 
 	for _, postPath := range posts {
@@ -81,13 +113,27 @@ func doPush(ctx context.Context, posts []string, opts pushOpts) error {
 			return fmt.Errorf("open post %q: %w", postPath, err)
 		}
 
-		var matter postMetadata
-		rest, err := frontmatter.MustParse(postFile, &matter)
+		p, err := parsePost(postFile)
 		if err != nil {
-			return fmt.Errorf("invalid post: %q: %w", postPath, err)
+			return fmt.Errorf("parse post %q: %w", postPath, err)
 		}
-		fmt.Printf("%+v\n", matter)
-		fmt.Println(string(rest))
+
+		// fmt.Printf("%+v\n", p)
+
+		msg := vm.MsgCall{
+			Caller:  caller,
+			PkgPath: opts.PkgPath,
+			Func:    "Render",
+			Args:    []string{"p/" + p.Slug},
+		}
+		tx := std.Tx{
+			Msgs:       []std.Msg{msg},
+			Fee:        std.NewFee(opts.GasWanted, gasFee),
+			Signatures: nil,
+			// Memo:
+		}
+
+		log.Println(string(amino.MustMarshalJSON(tx)))
 	}
 
 	return nil
--- gnoblog-cli/go.mod
+++ gnoblog-cli/go.mod
@@ -4,12 +4,52 @@ go 1.19
 
 require (
 	github.com/adrg/frontmatter v0.2.0
+	github.com/gnolang/gno v0.0.0-20230202075547-bd13cf76348a
 	github.com/peterbourgon/ff/v3 v3.3.0
 )
 
 require (
 	github.com/BurntSushi/toml v0.3.1 // indirect
-	github.com/kr/text v0.2.0 // indirect
-	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	github.com/btcsuite/btcd v0.22.0-beta.0.20220111032746-97732e52810c // indirect
+	github.com/btcsuite/btcd/btcutil v1.1.1 // indirect
+	github.com/cespare/xxhash v1.1.0 // indirect
+	github.com/cespare/xxhash/v2 v2.1.1 // indirect
+	github.com/cockroachdb/apd v1.1.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dgraph-io/badger/v3 v3.2103.4 // indirect
+	github.com/dgraph-io/ristretto v0.1.1 // indirect
+	github.com/dustin/go-humanize v1.0.0 // indirect
+	github.com/gnolang/cors v1.8.1 // indirect
+	github.com/gnolang/overflow v0.0.0-20170615021017-4d914c927216 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b // indirect
+	github.com/golang/groupcache v0.0.0-20190702054246-869f871628b6 // indirect
+	github.com/golang/protobuf v1.5.0 // indirect
+	github.com/golang/snappy v0.0.3 // indirect
+	github.com/google/flatbuffers v1.12.1 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/jmhodges/levigo v1.0.0 // indirect
+	github.com/klauspost/compress v1.12.3 // indirect
+	github.com/kr/pretty v0.2.1 // indirect
+	github.com/libp2p/go-buffer-pool v0.1.0 // indirect
+	github.com/linxGnu/grocksdb v1.7.2 // indirect
+	github.com/pelletier/go-toml v1.9.5 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.7.1 // indirect
+	github.com/syndtr/goleveldb v1.0.0 // indirect
+	github.com/tecbot/gorocksdb v0.0.0-20191217155057-f0fad39f321c // indirect
+	go.etcd.io/bbolt v1.3.6 // indirect
+	go.opencensus.io v0.22.5 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.8.0 // indirect
+	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
+	golang.org/x/mod v0.7.0 // indirect
+	golang.org/x/net v0.5.0 // indirect
+	golang.org/x/sys v0.4.0 // indirect
+	golang.org/x/term v0.4.0 // indirect
+	golang.org/x/tools v0.5.0 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20220512140231-539c8e751b99 // indirect
 )
