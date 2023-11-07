98a2d70d45f76e8d8395e58d6dbac52de22d7497
leohhhn <hudakleon@gmail.com>
2023-11-07 15:48:14 +0100

    fix links in q3 funding

--- posts/thumbs.sh
+++ posts/thumbs.sh
@@ -1,10 +1,15 @@
 #!/bin/sh -x
 
-for dir in */; do (
-    cd $dir
-    mkdir -p thumbs
+for dir in */; do
+    if [ -d "${dir}src" ]; then
+        mkdir -p "${dir}src/thumbs"
+        cd "${dir}src" || continue
 
-    for img in *.png *.jpg; do
-        convert $img -resize 600x ./thumbs/$img
-    done
-); done
+        for img in *.png *.jpg; do
+            sips --resampleWidth 600 "$img" --out "thumbs/$img"
+        done
+
+        cd ../..
+    fi
+done
+>>>>>>> Stashed changes
