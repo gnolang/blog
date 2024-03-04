2928fb2179c621622a4300e194af668d9aa1f398
leohhhn <hudakleon@gmail.com>
2024-03-04 21:21:37 +0100

    updat err msg again

--- cmd/gnoblog-cli/errors.go
+++ cmd/gnoblog-cli/errors.go
@@ -3,6 +3,6 @@ package main
 import "errors"
 
 var (
-	ErrInvalidNumberOfArgs = errors.New("invalid number of args; please input only 1 argument")
+	ErrInvalidNumberOfArgs = errors.New("invalid number of args; please input exactly 1 argument")
 	ErrEmptyKeyName        = errors.New("key name cannot be empty")
 )
